bc115de68c1dd8d51756361fa4617611f7d9019f
dhoncharenko <dhoncharenko@lohika.com>
2018-07-31 20:45:20 +0300

    created structure

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false
+    };
+  }
+
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+
+  // https://medium.com/react-native-development/how-to-use-the-flatlist-component-react-native-basics-92c482816fe6
+  getUsers(since, perPage) {
+    return fetch(`https://api.github.com/users?per_page=${perPage || ''}&since=${since || ''}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <List>
+          <FlatList
+            data={this.state.data}
+          />
+        </List>
       </View>
     );
   }
@@ -18,6 +48,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
